8b9885bf70cf616d11679060ae5d32c1d43c47e4
IoTKETI <iot.keti@gmail.com>
2023-03-09 14:17:52 +0900

    - 로그 추가

--- rc_hub.js
+++ rc_hub.js
@@ -163,7 +163,7 @@ function rcPortData(message) {
             if (flight.simul.toLowerCase() === 'off') {
                 // to real
                 rfPort.write(Buffer.from(RCData, 'hex'), () => {
-                    console.log('Send to real drone');
+                    console.log('Send ' + RCData + ' to real drone');
                 });
             }
 
